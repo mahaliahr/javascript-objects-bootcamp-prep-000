8351602b36f4dda862fd28eb267013f2652e52d2
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-15 22:16:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
-var playlist = {'bonobo':"migration"};
+var playlist = { 'bonobo':"migration" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = songTitle;
-  return playlist
+  return playlist.ar
 }
 
-function removeFromPlaylist(playlist, artist) {
+function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
   return playlist
 }
